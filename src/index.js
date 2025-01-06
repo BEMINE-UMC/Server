3ef2787effc87c67c9b8bc398c0f3338fd8119d4
TaeHun <xogns991124@naver,com>
2025-01-06 13:26:30 +0900

    init: project setting

---
+++ src/index.js
@@ -0,0 +1,95 @@
+import dotenv from "dotenv";
+import express from 'express'
+import cors from 'cors';
+import swaggerUiExpress from "swagger-ui-express";
+import swaggerAutogen from "swagger-autogen";
+
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+
+/*****************공통 응답을 사용할 수 있는 헬퍼 함수 등록*********************/
+app.use((req, res, next) => {
+    res.success = (success) => {
+        return res.json({
+            resultType: "SUCCESS",
+            error: null,
+            success: success,
+        });
+    };
+
+    res.error = ({ errorCode = "unknown", reason = null, data = null }) => {
+        return res.json({
+            resultType: "FAIL",
+            error: { errorCode, reason, data },
+            success: null,
+        });
+    };
+
+    next();
+});
+/*****************공통 응답을 사용할 수 있는 헬퍼 함수 등록*********************/
+
+
+app.use(cors());
+app.use(express.static('public'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false}));
+app.use(
+    "/docs",
+    swaggerUiExpress.serve,
+    swaggerUiExpress.setup(
+        {},
+        {
+            swaggerOptions: {
+                url: "/openapi.json",
+            },
+        }
+    )
+);
+app.get("/openapi.json", async (req, res, next) => {
+    // #swagger.ignore = true
+    const options = {
+        openapi: "3.0.0",
+        disableLogs: true,
+        writeOutputFile: false,
+    };
+    const outputFile = "/dev/null"; // 파일 출력은 사용하지 않습니다.
+    const routes = ["./src/index.js"];
+    const doc = {
+        info: {
+            title: "BEMINE",
+            description: "BEMINE 프로젝트",
+        },
+        host: `localhost:${3000}`,
+    };
+
+    const result = await swaggerAutogen(options)(outputFile, routes, doc);
+    res.json(result ? result.data : null);
+});
+
+
+app.get('/',(req,res)=>{{
+    res.send("hello world!")
+}})
+
+/****************전역 오류를 처리하기 위한 미들웨어*******************/
+app.use((err, req, res, next) => {
+    if (res.headersSent) {
+        return next(err);
+    }
+    console.log(err);
+    res.status(err.statusCode || 500).error({
+        errorCode: err.errorCode || "unknown",
+        reason: err.reson || err.message || null,
+        data: err.data || null,
+    });
+});
+/****************전역 오류를 처리하기 위한 미들웨어*******************/
+
+
+app.listen(port, ()=>{
+    console.log(`Example app listening on port ${port}`);
+})
