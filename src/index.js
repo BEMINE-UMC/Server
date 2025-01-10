974c8bfee01da518403d601565ed924d176bf1e2
jangyeeunee <amy5132@naver.com>
2025-01-11 04:55:01 +0900

    템플릿 좋아요 누르기  API 스웨거문서

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { getPopularTemplates } from './controllers/popular.template.controller.j
 import { handleOtherPost, handlerGetUserPost, handlerPostLike, handlerPostScrapt, handlerPostSearch,  } from "./controllers/post.controller.js";
 import {handlerGetUserHistory, handlerPatchMyProfile} from "./controllers/user.controller.js";
 import {handlerGetRecentPost, handlerGetScrapPost, } from "./controllers/post.controller.js";
-import {handlerGetTempleteView} from "./controllers/template.controller.js";
+import {handlerCreateTemplateLike, handlerGetTempleteView} from "./controllers/template.controller.js";
 import { handleViewAllPosts } from "./controllers/post.controller.js";
 import { handleFullTemplateLoad, handleTemplateDelete, handleTemplateCreateAndModify } from "./controllers/template.controller.js";
 import { handleViewTemplate } from "./controllers/template-view.controller.js";
@@ -163,6 +163,8 @@ app.get('/api/v1/posts/search',handlerPostSearch);
 //작성한 게시물 조회 API
 app.get('/api/v1/users/:userId/mypage/posts',handlerGetUserPost)
 
+//템플릿 좋아요 누르기 API
+app.post('/api/v1/users/:userId/templates/:templateId/like',handlerCreateTemplateLike)
 /****************전역 오류를 처리하기 위한 미들웨어*******************/
 app.use((err, req, res, next) => {
     if (res.headersSent) {
