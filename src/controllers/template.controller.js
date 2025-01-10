7eb618d7c5b9d8e537f22bb8c00779b20c918b34
Ga Min Cha <prettygamin@naver.com>
2025-01-10 18:16:02 +0900

    docs: Swagger 수정2 (차차 담당)

--- src/controllers/template.controller.js
+++ src/controllers/template.controller.js
@@ -3,6 +3,65 @@ import { fullTemplateLoad } from "../services/template.service.js";
 
 // 템플릿 전체 불러오기 요청
 export const handleFullTemplateLoad = async (req, res, next) => {
+    /* 
+    #swagger.summary = '템플릿 전체 불러오기 API';
+    #swagger.tags = ['Get']
+    #swagger.description = '템플릿 전체를 불러오는 API입니다.'
+    #swagger.responses[200] = {
+        description: "템플릿 전체 불러오기 성공 응답",
+        content: {
+            "application/json": {
+                schema: {
+                    type: "object",
+                    properties: {
+                        resultType: { type: "string", example: "SUCCESS" },
+                        error: { type: "object", nullable: true, example: null },
+                        success: {
+                            type: "object",
+                            properties: {
+                                templateId: { type: "number", example: 1 },
+                                userId: { type: "number", example: 1 },
+                                title: { type: "string", example: "Portfolio1 Template" },
+                                file: { type: "string", example: "https://example.com/files/template1.pptx" },
+                                fileShareState: { type: "string", example: "public" },
+                                thumbnail: { type: "string", example: "https://example.com/images/thumb1.jpg" },
+                                createdAt: { type: "string", format: "date", example: "2025-01-10T00:41:23.000Z" },
+                                updatedAt: { type: "string", format: "date", example: "2025-01-10T00:41:23.000Z" }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    #swagger.responses[400] = {
+        description: "템플릿 전체 불러오기 실패 응답",
+        content: {
+            "application/json": {
+                schema: {
+                    type: "object",
+                    properties: {
+                        resultType: { type: "string", example: "FAIL" },
+                        error: {
+                            type: "object",
+                            properties: {
+                                errorCode: { type: "string", example: "T001" },
+                                reason: { type: "string", example: "유효하지 않은 templateId 입니다." },
+                                data: {
+                                    type: "object",
+                                    properties: {
+                                        requestedTempalteId: { type: "number", example: 0 }
+                                    }
+                                }
+                            }
+                        },
+                        success: { type: "object", nullable: true, example: null }
+                    }
+                }
+            }
+        }
+    }
+    */
     try {
         console.log("\n템플릿 전체 불러오기를 요청했습니다!");
         console.log(`요청된 템플릿 아이디입니다: ${req.params.templateId}`);
@@ -18,6 +77,8 @@ export const handleFullTemplateLoad = async (req, res, next) => {
 export const handleTemplateDelete = async (req, res, next) => {
     /* 
     #swagger.summary = '템플릿 삭제 API';
+    #swagger.tags = ['Delete']
+    #swagger.description = '템플릿을 삭제하는 API입니다.'
     #swagger.responses[200] = {
         description: "템플릿 삭제 성공 응답",
         content: {
@@ -40,7 +101,7 @@ export const handleTemplateDelete = async (req, res, next) => {
         }
     }
     #swagger.responses[400] = {
-        desciption: "템플릿 삭제 실패 응답",
+        description: "템플릿 삭제 실패 응답",
         content: {
             "application/json": {
                 schema: {
@@ -78,6 +139,8 @@ export const handleTemplateDelete = async (req, res, next) => {
 export const handleTemplateCreateAndModify = async (req, res, next) => {
     /* 
     #swagger.summary = '템플릿 수정/생성 API';
+    #swagger.tags = ['Put']
+    #swagger.description = '템플릿을 수정/생성 하는 API입니다.'
     #swagger.requestBody = {
         required: true,
         content: {
@@ -122,7 +185,7 @@ export const handleTemplateCreateAndModify = async (req, res, next) => {
         }
     }
     #swagger.responses[400] = {
-        desciption: "템플릿 수정/생성 실패 응답",
+        description: "템플릿 수정/생성 실패 응답",
         content: {
             "application/json": {
                 schema: {
